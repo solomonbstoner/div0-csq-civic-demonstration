10116e200cd17a6ff2ea1f1e2091d71a6d862b88
Solomon Tan <solomonbstoner@protonmail.ch>
2019-06-16 00:09:48 +0800

    Added more features
    I made the following changes:
    1. Fix bug in timing. `usleep` takes in micro seconds, not milli seconds
    2. Added the feature to turn off the airbag warning lights
    
    Signed-off-by: Solomon Tan <solomonbstoner@protonmail.ch>

--- main.c
+++ main.c
@@ -16,6 +16,29 @@
 #define NO_OF_MENU_OPTIONS (6+1)
 #define CAN_NETWORK_NAME "slcan0" //define it to be a specific name for testing
 
+enum Warning_lights{
+	AIRBAGS_WARN = 0x0,
+	ENG_CHECK_WARN = 0x1,
+	ENG_OIL_WARN = 0x2,
+	SEATBELT_WARN = 0x3,
+	ABS_WARN = 0x4,
+	PARK_BRAKE_WARN = 0x5,
+	BATT_WARN = 0x6,
+	END_OF_WARN
+};
+
+struct _warning_lights_options {
+	int option_id;
+	char *option_string;
+	int can_msg_id;
+	int can_msg_len;
+	int heartbeat_bytes[4];
+	int byte_to_turn_on;
+	int byte_to_turn_off;
+	int byte_position;
+	int send_intervals_ms;
+};
+
 typedef int Can_socket;
 
 struct _main_menu_options{
@@ -24,12 +47,80 @@ struct _main_menu_options{
 	char *menu_string;
 };
 
+struct _warning_lights_options warning_lights_options_array[2] = {
+	{AIRBAGS_WARN, "[0] Airbag warning light\n", 0x39, 0x2, {0x39, 0x0C, 0x1B, 0x2A}, 0x00, 0x00, 0x00, 20},
+	{END_OF_WARN, NULL, 0x0, 0x0, {0x0, 0x0, 0x0, 0x0}, 0x00, 0x00, 0x00, 0}
+};
+
 void user_change_warn_lights(Can_socket s){
+
+	int user_on_off_option, user_warn_lights_option;
+
+	struct canfd_frame warning_light_frame; /*TODO: Is there a need to malloc space for this?*/
+	int enable_canfd = 1;
+	int mtu;
+	int required_mtu = CAN_MTU; /**/
+	int max_can_byte;
+
 	printf("\n\n\nTODO: Implement how to turn on/off warning lights\n\n\n");
+
+	/*TODO: Ask user to choose what lights to turn on/off*/
+
+	printf( "\nWhich warning light?\n");
+	int i = 0;
+	do{
+		printf("%s", warning_lights_options_array[i].option_string);
+		i++;
+	} while(warning_lights_options_array[i].option_string != NULL);
+	printf("Your choice: ");
+
+	if(scanf("%d", &user_warn_lights_option) <= 0){
+		printf("Illegal input.\n");
+		return;
+	}
+	if(user_warn_lights_option < 0 || user_warn_lights_option > 1){
+		/*Invalid option*/
+		printf("Illegal option.\n");
+		return;
+	}
+
+	printf("\nTurn on or off?\n"
+		"[0] Off\n"
+		"[1] On\n"
+		"Your choice: ");
+	if(scanf("%d", &user_on_off_option) <= 0){
+		printf("Illegal input.\n");
+		return;
+	}
+	if(user_on_off_option < 0 || user_on_off_option > 1){
+		/*Invalid option*/
+		printf("Illegal option.\n");
+		return;
+	}
+	/*TODO: Prepare can frame to be sent*/
+	struct _warning_lights_options *pntr_to_selected_warn_light = &(warning_lights_options_array[user_warn_lights_option]);
+	warning_light_frame.can_id = pntr_to_selected_warn_light->can_msg_id;
+	warning_light_frame.len = pntr_to_selected_warn_light->can_msg_len;
+	warning_light_frame.data[pntr_to_selected_warn_light->byte_position] = (user_on_off_option == 1) ? pntr_to_selected_warn_light->byte_to_turn_on : pntr_to_selected_warn_light->byte_to_turn_off;
+	
+	int heartbeat_index = 0;
+
+	/*TODO: Prepare the heartbeat*/
+	for(int i = 0; i < 20000; i++){
+		warning_light_frame.data[pntr_to_selected_warn_light->can_msg_len-1] = pntr_to_selected_warn_light->heartbeat_bytes[heartbeat_index];
+		if (write(s, &warning_light_frame, required_mtu) != required_mtu) {
+			perror("write");
+		}
+		usleep(pntr_to_selected_warn_light->send_intervals_ms * 1000);
+		heartbeat_index++;
+		heartbeat_index %= 4;
+	}
 }
+
 void user_change_rpm(Can_socket s){
 	printf("\n\n\nTODO: Implement how to change rpm\n\n\n");
 }
+
 void user_change_speed(Can_socket s){
 
 	struct canfd_frame speed_frame; /*TODO: Is there a need to malloc space for this?*/
@@ -88,7 +179,7 @@ void user_change_speed(Can_socket s){
 		if (write(s, &speed_frame, required_mtu) != required_mtu) {
 			perror("write");
 		}
-		sleep(0.010);
+		usleep(10 * 1000);
 		heartbeat_index++;
 		heartbeat_index %= 4;
 	}
@@ -97,7 +188,7 @@ void user_change_speed(Can_socket s){
 		if (write(s, &speed_frame, required_mtu) != required_mtu) {
 			perror("write");
 		}
-		sleep(0.010);
+		usleep(10 * 1000);
 	}
 
 	/*TODO: Find a way to keep it at the specified speed until user quits*/
@@ -108,22 +199,24 @@ void user_change_speed(Can_socket s){
 void user_change_gear(Can_socket s){
 	printf("\n\n\nTODO: Implement how to change the displayed gear\n\n\n");
 
-	struct canfd_frame speed_frame; /*TODO: Is there a need to malloc space for this?*/
+	/*TODO: Ask user what gear he wants to spoof.*/
+
+	struct canfd_frame gear_frame; /*TODO: Is there a need to malloc space for this?*/
 	int enable_canfd = 1;
 	int mtu;
 	int required_mtu = CAN_MTU; /**/
 	int max_can_byte;
-	int user_defined_speed;
 
-	memset(&speed_frame, 0, sizeof(speed_frame));
-	speed_frame.can_id = 0x188;
-	speed_frame.len = 6;
+	memset(&gear_frame, 0, sizeof(gear_frame));
+	gear_frame.can_id = 0x188;
+	gear_frame.len = 6;
 
-	int heartbeat_bytes[4] = {0x33, 0x06, 0x15, 0x24};
+	int heartbeat_bytes[4] = {0x34, 0x07, 0x16, 0x22};
 	int heartbeat_index = 0;
-
-	/*TODO: for loop to increment/decrement the speed*/
-	for(int i = 0; i < 2000/*TODO: 20 sec for testing */; i++){
+	gear_frame.data[3] = 0x01; /*TODO: P gear for testing*/
+	gear_frame.data[0] = 0x07; /*TODO: D gear for testing*/
+	gear_frame.data[2] = 0x80;
+	for(int i = 0; i < 20000/*TODO: 20 sec for testing */; i++){
 		/*
 		if(heartbeat_index==0){
 			int first_hb_byte = (rand() % 10) + 0x00;
@@ -142,12 +235,12 @@ void user_change_gear(Can_socket s){
 			heartbeat_bytes[6] = seventh_hb_byte;
 		}
 		*/
-		speed_frame.data[3] = 0x01; /*TODO: P gear for testing*/
-		speed_frame.data[5] = heartbeat_bytes[heartbeat_index];
-		if (write(s, &speed_frame, required_mtu) != required_mtu) {
+
+		gear_frame.data[5] = heartbeat_bytes[heartbeat_index];
+		if (write(s, &gear_frame, required_mtu) != required_mtu) {
 			perror("write");
 		}
-		sleep(0.010);
+		usleep(10 * 1000);
 		heartbeat_index++;
 		heartbeat_index %= 4;
 	}
@@ -184,7 +277,7 @@ void increment_odometer(Can_socket s){
 
 	for(int repeat = 0; repeat < 10; repeat++){
 		for(int i = 0; i <= 0xFF /*TODO: To replace with whatever bytes correspond to the speed*/; i++){
-			for(int j = 0; j < 200; j++){
+			for(int j = 0; j < 20; j++){
 				if(heartbeat_index==0){
 					int first_hb_byte = (rand() % 10) + 0x20;
 					int sec_hb_byte = (rand() % 10) + 0x30;
@@ -200,7 +293,7 @@ void increment_odometer(Can_socket s){
 				if (write(s, &odometer_frame, required_mtu) != required_mtu) {
 					perror("write");
 				}
-				sleep(0.010);
+				usleep(10 * 1000);
 				heartbeat_index++;
 				heartbeat_index %= 4;
 			}
@@ -221,7 +314,7 @@ struct _main_menu_options main_menu_options_array[NO_OF_MENU_OPTIONS] = {
 static int print_main_menu(void){
 	int i = 0;
 
-	printf( "What would you like to do?\n");
+	printf( "\nWhat would you like to do?\n");
 	do{
 		printf("%s", main_menu_options_array[i].menu_string);
 		i++;
@@ -239,6 +332,7 @@ int main(int argc, char **argv){
 	struct ifreq ifr;
 	
 	Can_socket s; /* can raw socket */
+
 
 	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
 		perror("socket");
