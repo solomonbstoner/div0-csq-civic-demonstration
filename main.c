acc1c24aaacdc874424b0dcdc931a74ad696906c
Solomon Tan <solomonbstoner@protonmail.ch>
2019-06-15 12:04:10 +0800

    Add features & improvements
    I made the following changes/improvements:
    1. Allow user to spoof the odometer
    2. Allow user to specify what speed to display on the speedometer (still
    buggy af)
    3. Moved the part of the code where it connects to the CAN network to
    the `main` function so it'll be shared by the functions that are later
    called (e.g. user_change_rpm, user_change_speed etc etc)
    
    Signed-off-by: Solomon Tan <solomonbstoner@protonmail.ch>

--- main.c
+++ main.c
@@ -13,30 +13,31 @@
 #include "lib.h"
 
 
-#define NO_OF_MENU_OPTIONS (5+1)
+#define NO_OF_MENU_OPTIONS (6+1)
 #define CAN_NETWORK_NAME "slcan0" //define it to be a specific name for testing
+
+typedef int Can_socket;
 
 struct _main_menu_options{
 	int option_id;
-	void (*function_to_execute)(void);
+	void (*function_to_execute)(Can_socket);
 	char *menu_string;
 };
 
-void user_change_warn_lights(void){
+void user_change_warn_lights(Can_socket s){
 	printf("\n\n\nTODO: Implement how to turn on/off warning lights\n\n\n");
 }
-void user_change_rpm(void){
+void user_change_rpm(Can_socket s){
 	printf("\n\n\nTODO: Implement how to change rpm\n\n\n");
 }
-void user_change_speed(void){
+void user_change_speed(Can_socket s){
 
 	struct canfd_frame speed_frame; /*TODO: Is there a need to malloc space for this?*/
 	int enable_canfd = 1;
 	int mtu;
 	int required_mtu = CAN_MTU; /**/
-	struct sockaddr_can addr;
-	struct ifreq ifr;
-	int s; /* can raw socket */
+	int max_can_byte;
+
 	int user_defined_speed;
 
 	printf("\n\n\nTODO: Implement how to change the speed\n\n\n");	
@@ -52,34 +53,13 @@ void user_change_speed(void){
 		return;
 	}
 
+	max_can_byte = (int) (user_defined_speed * 80000 / 621); /*TODO: Fix the formula*/
+
 	/*set up the can data frame*/
 	/*set timer interrupt to send packet every 10ms*/
 	/*increament the speed on the can data frame until the specified speed*/
 	/* TODO: Make sense of the initialisation from cansend */
 
-	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-		perror("socket");
-		fprintf(stderr, "Error with socket.\n");
-		return;
-	}
-
-	strncpy(ifr.ifr_name, CAN_NETWORK_NAME, IFNAMSIZ - 1);
-	ifr.ifr_name[IFNAMSIZ - 1] = '\0';
-	ifr.ifr_ifindex = if_nametoindex(ifr.ifr_name);
-	if (!ifr.ifr_ifindex) {
-		perror("if_nametoindex");
-		return;
-	}
-
-	memset(&addr, 0, sizeof(addr));
-	addr.can_family = AF_CAN;
-	addr.can_ifindex = ifr.ifr_ifindex;
-	setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
-
-	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-		perror("bind");
-		return;
-	}
 	
 	memset(&speed_frame, 0, sizeof(speed_frame));
 	speed_frame.can_id = 0x158;
@@ -87,13 +67,14 @@ void user_change_speed(void){
 
 	int heartbeat_bytes[4];
 	int heartbeat_index = 0;
+
 	/*TODO: for loop to increment/decrement the speed*/
-	for(int i = 0; i < 0xFFFF/*TODO: To replace with whatever bytes correspond to the speed*/; i++){
+	for(int i = 0; i < max_can_byte/*TODO: To replace with whatever bytes correspond to the speed*/; i++){
 		if(heartbeat_index==0){
-			int first_hb_byte = rand() % 10 + 20;
-			int sec_hb_byte = rand() % 10 + 30;
-			int third_hb_byte = rand() % 10 + 0;
-			int fourth_hb_byte = rand() % 10 + 10;
+			int first_hb_byte = (rand() % 10) + 0x20;
+			int sec_hb_byte = (rand() % 10) + 0x30;
+			int third_hb_byte = (rand() % 10) + 0x0;
+			int fourth_hb_byte = (rand() % 10) + 0x10;
 			heartbeat_bytes[0] = first_hb_byte;
 			heartbeat_bytes[1] = sec_hb_byte;
 			heartbeat_bytes[2] = third_hb_byte;
@@ -107,9 +88,16 @@ void user_change_speed(void){
 		if (write(s, &speed_frame, required_mtu) != required_mtu) {
 			perror("write");
 		}
-		sleep(0.10);
+		sleep(0.010);
 		heartbeat_index++;
 		heartbeat_index %= 4;
+	}
+
+	for(int i = 0; i < 500; i++){
+		if (write(s, &speed_frame, required_mtu) != required_mtu) {
+			perror("write");
+		}
+		sleep(0.010);
 	}
 
 	/*TODO: Find a way to keep it at the specified speed until user quits*/
@@ -117,23 +105,117 @@ void user_change_speed(void){
 
 }
 
-void user_change_gear(void){
+void user_change_gear(Can_socket s){
 	printf("\n\n\nTODO: Implement how to change the displayed gear\n\n\n");
+
+	struct canfd_frame speed_frame; /*TODO: Is there a need to malloc space for this?*/
+	int enable_canfd = 1;
+	int mtu;
+	int required_mtu = CAN_MTU; /**/
+	int max_can_byte;
+	int user_defined_speed;
+
+	memset(&speed_frame, 0, sizeof(speed_frame));
+	speed_frame.can_id = 0x188;
+	speed_frame.len = 6;
+
+	int heartbeat_bytes[4] = {0x33, 0x06, 0x15, 0x24};
+	int heartbeat_index = 0;
+
+	/*TODO: for loop to increment/decrement the speed*/
+	for(int i = 0; i < 2000/*TODO: 20 sec for testing */; i++){
+		/*
+		if(heartbeat_index==0){
+			int first_hb_byte = (rand() % 10) + 0x00;
+			int sec_hb_byte = (rand() % 10) + 0x00;
+			int third_hb_byte = (rand() % 10) + 0x10;
+			int fourth_hb_byte = (rand() % 10) + 0x10;
+			int fifth_hb_byte = (rand() % 10) + 0x20;
+			int sixth_hb_byte = (rand() % 10) + 0x20;
+			int seventh_hb_byte = (rand() % 10) + 0x30;
+			heartbeat_bytes[0] = first_hb_byte;
+			heartbeat_bytes[1] = sec_hb_byte;
+			heartbeat_bytes[2] = third_hb_byte;
+			heartbeat_bytes[3] = fourth_hb_byte;
+			heartbeat_bytes[4] = fifth_hb_byte;
+			heartbeat_bytes[5] = sixth_hb_byte;
+			heartbeat_bytes[6] = seventh_hb_byte;
+		}
+		*/
+		speed_frame.data[3] = 0x01; /*TODO: P gear for testing*/
+		speed_frame.data[5] = heartbeat_bytes[heartbeat_index];
+		if (write(s, &speed_frame, required_mtu) != required_mtu) {
+			perror("write");
+		}
+		sleep(0.010);
+		heartbeat_index++;
+		heartbeat_index %= 4;
+	}
+
 }
 
-void quit_program(void){
+void quit_program(Can_socket s){
+	/*TODO: End program*/
 	exit(0);
 }
 
-typedef int Can_socket;
+void increment_odometer(Can_socket s){
+	struct canfd_frame odometer_frame; /*TODO: Is there a need to malloc space for this?*/
+	int enable_canfd = 1;
+	int mtu;
+	int required_mtu = CAN_MTU; /**/
+	int max_can_byte;
+
+	printf("\n\n\nTODO: Implement how to increase the odometer\n\n\n");	
+
+	/*set up the can data frame*/
+	/*set timer interrupt to send packet every 10ms*/
+	/*increament the speed on the can data frame until the specified speed*/
+	/* TODO: Make sense of the initialisation from cansend */
+
+	
+	memset(&odometer_frame, 0, sizeof(odometer_frame));
+	odometer_frame.can_id = 0x158;
+	odometer_frame.len = CAN_MAX_DLEN;
+
+	int heartbeat_bytes[4];
+	int heartbeat_index = 0;
+
+
+	for(int repeat = 0; repeat < 10; repeat++){
+		for(int i = 0; i <= 0xFF /*TODO: To replace with whatever bytes correspond to the speed*/; i++){
+			for(int j = 0; j < 200; j++){
+				if(heartbeat_index==0){
+					int first_hb_byte = (rand() % 10) + 0x20;
+					int sec_hb_byte = (rand() % 10) + 0x30;
+					int third_hb_byte = (rand() % 10) + 0x0;
+					int fourth_hb_byte = (rand() % 10) + 0x10;
+					heartbeat_bytes[0] = first_hb_byte;
+					heartbeat_bytes[1] = sec_hb_byte;
+					heartbeat_bytes[2] = third_hb_byte;
+					heartbeat_bytes[3] = fourth_hb_byte;
+				}
+				odometer_frame.data[6] = i & 0xFF;
+				odometer_frame.data[7] = heartbeat_bytes[heartbeat_index];
+				if (write(s, &odometer_frame, required_mtu) != required_mtu) {
+					perror("write");
+				}
+				sleep(0.010);
+				heartbeat_index++;
+				heartbeat_index %= 4;
+			}
+		}
+	}
+}
 
 struct _main_menu_options main_menu_options_array[NO_OF_MENU_OPTIONS] = {
 	{0x0, user_change_warn_lights, "[0] Turn on/off warning lights\n"},
 	{0x1, user_change_rpm, "[1] Change RPM value\n"},
 	{0x2, user_change_speed, "[2] Change the speed\n"},
 	{0x3, user_change_gear, "[3] Change the gear\n"},
-	{0x4, quit_program, "[4] Exit\n"},
-	{0x5, NULL, NULL}
+	{0x4, increment_odometer, "[4] Increment the odometer\n"},
+	{0x5, quit_program, "[5] Exit\n"},
+	{0x6, NULL, NULL}
 };
 
 static int print_main_menu(void){
@@ -153,6 +235,35 @@ int main(int argc, char **argv){
 	char tmp_chr;
 	void *function_to_execute;
 
+	struct sockaddr_can addr;
+	struct ifreq ifr;
+	
+	Can_socket s; /* can raw socket */
+
+	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+		perror("socket");
+		fprintf(stderr, "Error with socket.\n");
+		exit(1);
+	}
+
+	strncpy(ifr.ifr_name, CAN_NETWORK_NAME, IFNAMSIZ - 1);
+	ifr.ifr_name[IFNAMSIZ - 1] = '\0';
+	ifr.ifr_ifindex = if_nametoindex(ifr.ifr_name);
+	if (!ifr.ifr_ifindex) {
+		perror("if_nametoindex");
+		exit(1);
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+	setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("bind");
+		exit(1);
+	}
+
 	while(1/*program should run forever*/ ){
 		print_main_menu();
 		if(scanf("%d", &user_option) <= 0){
@@ -164,7 +275,7 @@ int main(int argc, char **argv){
 			printf("Illegal option.\n");
 			continue;
 		}
-		main_menu_options_array[user_option].function_to_execute();
+		main_menu_options_array[user_option].function_to_execute(s);
 		/*print options for user to choose from*/	
 		/*process user options*/
 	}
